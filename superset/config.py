407601641c8fce577007c98558c8346fe61ffa0b
John Bodley <john.bodley@gmail.com>
2024-05-21 09:58:44 -0700

    chore: Set isolation level as READ COMMITTED

--- superset/config.py
+++ superset/config.py
@@ -194,6 +194,10 @@ SQLALCHEMY_DATABASE_URI = (
 # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "isolation_level": "READ COMMITTED",
+}
+
 # In order to hook up a custom password store for all SQLALCHEMY connections
 # implement a function that takes a single argument of type 'sqla.engine.url',
 # returns a password and set SQLALCHEMY_CUSTOM_PASSWORD_STORE.
